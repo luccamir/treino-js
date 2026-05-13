8ac1fd354a8f5475eafdd870882ab7e573a7bddf
Lucca Rodrigues <luccamirandar@gmail.com>
2026-05-12 21:15:24 -0300

    adiciona exercício de treino em JS

---
+++ ttreino.js
@@ -0,0 +1,8 @@
+var nomeTurista;
+var valor;
+nomeTurista = prompt("Olá! Qual seu nome?");
+valor = Number(prompt("Quanto você traz em dinheiro?"));
+if (valor > 10){
+    alert("Aproveite os Quiosques de água de coco");
+}
+alert ("Bom passeio");
